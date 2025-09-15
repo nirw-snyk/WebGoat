584ff0f062f1ad30e42ac92507c5747a42144ba4
nirw-snyk <130424397+nirw-snyk@users.noreply.github.com>
2025-09-15 11:38:46 +1000

    Update ProfileUploadRetrieval.java

--- src/main/java/org/owasp/webgoat/lessons/pathtraversal/ProfileUploadRetrieval.java
+++ src/main/java/org/owasp/webgoat/lessons/pathtraversal/ProfileUploadRetrieval.java
@@ -77,12 +77,7 @@ public class ProfileUploadRetrieval extends AssignmentEndpoint {
     }
     return failed(this).build();
   }
-  // Returns the same string, no sanitization really
-  public String SanitizeParam(String input) {
-    return input;
- }
 
-    
   @GetMapping("/PathTraversal/random-picture")
   @ResponseBody
   public ResponseEntity<?> getProfilePicture(HttpServletRequest request) {
